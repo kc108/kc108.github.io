004470252e712a63cc89b21b939b578a82c33275
Kim Carpico <kcarpico@gmail.com>
2021-02-26 17:45:27 -0800

    got buttons working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // console.log("Finn cat")
 const API_KEY = 'bb7080f5-00e5-4645-9c7b-6d3eca4f0645';
-let catData, imageData, breeds, breed;
+let catData, imageData, breeds, correctBreed;
 
 const $breed = $("#breed");
 const $image = $("#image");
@@ -9,6 +9,8 @@ const $country = $("#country");
 const $weight = $("#weight");
 const $life_span = $("#life_span");
 const $desc = $("#desc");
+const $buttons = $(".breed");
+
 
 
 const promise = $.ajax({
@@ -28,11 +30,11 @@ const promise = $.ajax({
 
 function getImage() {
     let breedNum = Math.floor(Math.random() * Math.floor(breeds.length));
-    breed = breeds[breedNum];
+    correctBreed = breeds[breedNum];
     // console.log(breed);
 
     const promise2 = $.ajax({
-        url: 'https://api.thecatapi.com/v1/images/search?breed_id=' + breed.id,
+        url: 'https://api.thecatapi.com/v1/images/search?breed_id=' + correctBreed.id,
         headers: { "x-api-key": API_KEY }
     }).then(
         (data) => {
@@ -49,15 +51,68 @@ function getImage() {
 // console.log($breed);
 
 function render() {
-    $breed.text(breed.name);
+    $breed.text(correctBreed.name);
     // $image.attr(imageData);
     //console.log(breed);
     $image.attr('src', imageData[0].url);
-    $energy_level.text(breed.temperament);
-    $country.text(breed.origin);
-    $weight.text(breed.weight_imperial);
-    $life_span.text(breed.life_span);
-    $desc.text(breed.description);
+    $energy_level.text(correctBreed.temperament);
+    $country.text(correctBreed.origin);
+    $weight.text(correctBreed.weight_imperial);
+    $life_span.text(correctBreed.life_span);
+    $desc.text(correctBreed.description);
+    // select 3 wrong answers
+    let buttonBreeds = [correctBreed];
+    for (let i = 0; i < 3; i++) {
+       let wrongBreed = breeds[Math.floor(Math.random() * breeds.length)];
+       // console.log(wrongBreed);
+       buttonBreeds.push(wrongBreed);
+       
+    }
+
+    //console.log(buttonBreeds[0]);
+    shuffle(buttonBreeds);
+    // console.log(buttonBreeds[0]);
+    // name button with random cat breeds
+    for (let i = 0; i < 4; i++) {
+    $buttons.eq(i).text(buttonBreeds[i].name);
+    }
+    
+    // button inside of pop-up to go to next cat
+    // show number of correctly guessed cat breeds
+    
 }
+
+// when correct answer is selected, will pop-up with breed information
+$buttons.on('click', handleClick);
+function handleClick(event) {
+    // call preventDefault() 'onclick' event will refresh page
+    event.preventDefault();
+    if(correctBreed.name === $(event.target).text()) {
+        $("#pop_up").show();
+    } 
+    // console.log("finn")
+    
+}
+
+// will shuffle arrays for buttons: reference: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
 
 
